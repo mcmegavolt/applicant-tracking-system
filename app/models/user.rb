c68faaffcf5907b842619da015d0e0051c306e5c
Oleksandr Ponomarenko <mcmegavolt@gmail.com>
2024-05-14 13:47:08 +0300

    Setup devise jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :registerable
   # :omniauthable, :recoverable, :rememberable, :validatable
-  devise :database_authenticatable
+  devise :database_authenticatable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
